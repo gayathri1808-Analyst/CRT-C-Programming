e96c43b32ae4ea95c46702c339e5963c932aa331
NKN Gayathri <gayathrinaidu3660@gmail.com>
2026-07-30 06:30:24 +0530

    DivisibleAndNonDivisibleSumsDifference.c

---
+++ Day-02/DivisibleAndNonDivisibleSumsDifference.c
@@ -0,0 +1,10 @@
+int differenceOfSums(int n, int m) {
+    int num1 = 0;
+    int num2 = 0;
+
+    for(int i = 1; i <= n; i++)
+    {
+        (i % m == 0) ? (num2 += i) : (num1 += i);
+    }
+    return num1 - num2;
+}
